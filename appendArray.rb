7b9b5417ba2a0d2520c0a66a851e9dd097d3dd95
My Mai <my.mai@asnet.com.vn>
2015-03-22 10:15:55 +0700

    append array

---
+++ appendArray.rb
@@ -0,0 +1,17 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Append '11' to the end of the original array
+
+puts "Append '11' to the end of the original array"
+
+array.push(11);
+
+puts array
+
+# Append '0' to the beginning the original array
+
+puts "Append '0' to the beginning the original array"
+
+array.unshift(0);
+
+puts array
